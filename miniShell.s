700a99c8d64a9e0e77d9c8f2777f5a6e4a53865b
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-08-04 15:10:26 +0800

    Structure optimized and comments added

--- miniShell.s
+++ miniShell.s
@@ -7,25 +7,25 @@ _start:
 main:
     bl showPrompt
     bl readUserInput
-    bl displayUserInput
+    bl displayUserInput @ temporarily kept for testing
     bl executeCommand
     b main  @Infinity loop
 
 @ Ask user to prompt with outputing "$ " on the display
 @ Addtional parameters are not needed
 showPrompt:
-    push {lr}
+    push {r4-r11, lr}
     ldr r0, =prompt @"$ "
     bl printf
-    pop {pc} @ return
+    pop {r4-r11, pc} @ return
 
 @ Simulate C's printf function
-@ Take r0 as input
+@ Take r0 as input(pointer to the string)
 printf:
-    @push {lr} @ No use of volatile registers
+    @ push {lr} @ No use of volatile registers
     push {r4-r11, lr} @ fixed, make sure to follow arm convention
-    mov r1, r0  @ Load input string
-    bl strlen
+    mov r1, r0  @ Load input string tp r1
+    bl strlen   @ get the length of the string
     mov r2, r0  @ r2 = length of the given string
 
     @ write to stdout
@@ -41,39 +41,43 @@ printf:
 @ Take r0 as input
 strlen:
     push {r4-r11, lr}
-    mov r1, r0  @ Load input string
+    mov r1, r0  @ Load input string(pointer to the string)
     mov r2, #0  @ Length
 
-_strlenLoop:
-    ldrb r3, [r1, r2]
-    cmp r3, #0  @If == '\0'
-    beq _strlenLoopEnd
-    @ else
-    add r2, r2, #1  @length++
-    b _strlenLoop
+    @ iterate through the string
+    strlenLoop:
+        ldrb r3, [r1, r2]   @ load the data in the address of r1 offset by r2
+        cmp r3, #0
+        @ if == '\0'
+        beq strlenLoopEnd
+        @ else
+        add r2, r2, #1  @ length++
+        b strlenLoop
 
-_strlenLoopEnd:
-    mov r0, r2
-    pop {r4-r11, pc}
+    strlenLoopEnd:
+        mov r0, r2  @ return length
+        pop {r4-r11, pc}
 
 
+@ read user' s input by calling sys_read
 readUserInput:
     push {r4-r11, lr}
 
     @ read from stdin
     mov r0, #0  @ stdin fd
     ldr r1, =buffer @ pointer to the buffer
-    mov r2, #128    @ maximum load size
+    mov r2, #127    @ maximum load size(1 additional space for '\0')
     mov r7, #3  @ sys_read
     svc #0
 
-    @ add '\0' for null terminated(replacing '\n' with '\0')
+    @ add '\0' for null terminated(or replacing '\n' with '\0')
     @ move r1 to the end of the string(+1)
-    add r1, r1, r0  @r0 store the return value of numbers of bytes read
-    mov r2, #0
-    sub r3, r1, #1
-    ldrb r4, [r3]   @ note: we can't directly use cmp to compare value with registers
+    add r1, r1, r0  @ r0 store the return value of numbers of bytes read(return from sys_read)
+    mov r2, #0  @ for null terminated
+    sub r3, r1, #1  @ offset 1 address
+    ldrb r4, [r3]   @ note: we can't directly use cmp to compare value with registers, so we load it to a register
     cmp r4, #0xa    @ compare it with '\n'
+    @ if not '\n', directly add '\0'; otherwise, replace it
     bne endReadUserInput
         strb r2, [r3]
         pop {r4-r11, pc}
@@ -82,6 +86,7 @@ readUserInput:
         strb r2, [r1]
         pop {r4-r11, pc}
 
+@ prints the user input in the terminal(new line added for readability)
 displayUserInput:
     push {r4-r11, lr}
     ldr r0, =buffer
@@ -90,7 +95,7 @@ displayUserInput:
     bl printf
     pop {r4-r11, pc}
 
-
+@ handing commands with fork, child, parent and wait
 executeCommand:
     push {r4-r11, lr}
     ldr r0, =buffer
@@ -99,22 +104,15 @@ executeCommand:
 
     cmp r0, #-1  @ make sure the parse is done correctly
     beq endExecute
-    @ else
+    @ else, fork the process
     bl fork
-    cmp r0, #0  @ return 0 if a child process, pid a parent process
+    cmp r0, #0  @ fork returns 0 if a child process, pid a parent process
     beq child   @ if we're in child process, run child
     bl wait @ if we're in parent process, wait until child process ends
 
     endExecute:
         pop {r4-r11, pc}
 
-@TODO: parse the command
-@ returns 0 if parsed correctly
-parseCommand:
-    push {r4-r11, lr}
-    mov r0, r0 @ load the command
-
-    pop {r4-r11, pc}
 
 @ call sys_fork for forking
 fork:
@@ -124,25 +122,40 @@ fork:
     pop {r4-r11, pc}
 
 @ call sys_execve
+@ int execve(const char *filename, const char *const *argv, const char *const *envp);
+@ TODO::
 child:
     push {r4-r11, lr}
     ldr r0, =buffer
-    mov r1, #0
-    mov r2, #0  @ place for environment pointer
-    mov r7, #11  @ sys_execve
+    mov r1, #0  @ place for argv
+    mov r2, #0  @ place for envp(environment pointer)
+    mov r7, #0xb  @ sys_execve
     svc #0
 
-    @ only run if execve somehow failed
-    mov r7, #1
+    @ only run if execve somehow fails
+    mov r7, #1  @ sys_exit
     svc #0
 
-wait:
+@ call sys_wait for a parent
+@ wait4(int *stat_addr, int options, struct rusage *ru)
     mov r7, #0x72   @ sys_wait(for arm linux kernel, sys_wait4)
-    mov r0, #-1 @ wait for any child
+    @ wait for any child for any status
+    mov r0, #-1
     mov r1, #0  @ no options
     mov r2, #0  @ no status
+
     svc #0
     pop {r4-r11, pc}
+
+
+@TODO: parse the command
+@ returns 0 if parsed correctly
+parseCommand:
+    push {r4-r11, lr}
+    mov r0, r0 @ load the command
+
+    pop {r4-r11, pc}
+
 
 .section .data
 @ Used by showPrompt()
